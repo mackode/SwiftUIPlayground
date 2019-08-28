63c011cffce0567bdd9ddc4b8f11def25895f302
Bartłomiej Makowski <biuro@mackode.pl>
2019-08-28 21:49:02 +0200

    Initial setup and scene

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by Mackode - Bartlomiej Makowski on 28/08/2019.
+//  Copyright © 2019 pl.mackode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
