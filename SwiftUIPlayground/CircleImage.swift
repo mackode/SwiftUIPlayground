191a87d80d1d86c24d840c98985365be8eb75342
Bartłomiej Makowski <biuro@mackode.pl>
2019-09-02 22:37:11 +0200

    Creating and Combining Views

---
+++ SwiftUIPlayground/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  SwiftUIPlayground
+//
+//  Created by Mackode - Bartlomiej Makowski on 02/09/2019.
+//  Copyright © 2019 pl.mackode. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("fireball")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUIPlayground/Profile/Landmark/ProfileHost.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHost.swift
+//  SwiftUIPlayground
+//
+//  Created by Mackode - Bartlomiej Makowski on 02/09/2019.
+//  Copyright © 2019 pl.mackode. All rights reserved.
+//
+
+import SwiftUI
+
+/*
+struct ProfileHost: View {
+    @State var profile = Profile.default
+    var body: some View {
+        Text("Profile for: \(profile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
+*/
