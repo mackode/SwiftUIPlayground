21ecc3586f9dabcedc74597864b419f0f0b82c2c
Bartłomiej Makowski <biuro@mackode.pl>
2019-09-04 06:32:55 +0200

    Drawing Paths and Shapes

---
+++ SwiftUIPlayground/Badge.swift
@@ -0,0 +1,22 @@
+//
+//  Badge.swift
+//  SwiftUIPlayground
+//
+//  Created by Mackode - Bartlomiej Makowski on 03/09/2019.
+//  Copyright © 2019 pl.mackode. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
